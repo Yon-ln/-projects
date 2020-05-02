56f40f68775c59385f8ae407f21d159403981389
yon <yon@users.noreply.github.com>
2020-05-03 00:33:15 +0100

    asdasdadasd
    asdasdsadsadasdasdasdsadaasdas

--- javascript.js
+++ javascript.js
@@ -5,14 +5,16 @@ var work = new Array();
 
 var clicked = false;
 
+var tempHover = '#Showcase .box:hover{opacity:0.9;width:calc(25%);margin-bottom:calc(7.5%);transition: opacity .25s ease-in-out, margin-bottom .5s, width .25s ease-in-out;}'
+
 
 
 work['games'] = new Array();
 
-work['games'][0] = ["Doctor Gear", "./assets/doctor gear icon.png", false];
-work['games'][1] = ["Dungeon Crawler", "./assets/dungeon crawler icon.png", false];
-work['games'][2] = ["The Last Hope", "./assets/the last hope icon.png", false];
-work['games'][3] = ["Romance Story", "./assets/romance story icon.png", false];
+work['games'][0] = ["Doctor Gear", "./assets/doctor gear icon.png", false, "1600px", "900px"];
+work['games'][1] = ["Dungeon Crawler", "./assets/dungeon crawler icon.png", false, "600px", "600px"];
+work['games'][2] = ["The Last Hope", "./assets/the last hope icon.png", false, "1600px", "900px"];
+work['games'][3] = ["Romance Story", "./assets/romance story icon.png", false, "1600px", "900px"];
 
 
 function showGame(index, game){
@@ -45,6 +47,7 @@ for(var i = 0; i < Object.keys(work['games']).length; i++){
 
 
 		work['games'][i].push(game);
+		work['games'][i].push(div);
 
 		div.addEventListener('click',(function (i) {
           return function () {
@@ -53,15 +56,23 @@ for(var i = 0; i < Object.keys(work['games']).length; i++){
 				
 				if(work['games'][x] != work['games'][i]){
 					console.log(x);
-					work['games'][x][3].style.opacity = "0";
-					work['games'][x][3].style.zIndex = "-1";
-					work['games'][x][3].style.transition = "opacity .25s";
+
+					game.position = "fixed";
+					work['games'][x][5].style.width = 0;
+					work['games'][x][5].style.height = 0;
+					work['games'][x][5].style.opacity = "0";
+					work['games'][x][5].style.zIndex = "-1";
+					work['games'][x][5].style.transition = "opacity .25s";
+
 				} 
 			}
-
+			game.position = "fixed";
+			game.style.width = work['games'][i][3];
+			game.style.height = work['games'][i][4];
 			game.style.opacity = "1";
-			game.style.zIndex = "1";
-			game.style.transition = "opacity .25s";
+			game.style.zIndex = "5";
+			game.style.transition = "opacity .25s, width .25s, height .25s";
+
           }
         }(i)));
 
