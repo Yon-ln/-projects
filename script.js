568a9058f5c1cb553a4ecfbfd2e6aa4ace84d5f1
YON <dlncg4602@gmail.com>
2021-11-04 05:30:03 +0000

    good category switch
    individual item names are next

--- script.js
+++ script.js
@@ -4,11 +4,10 @@ var category = document.querySelectorAll(".categories")
 
 var scrollheight = document.getElementById("s").scrollHeight;
 
-
 var categories = {
-"Games" : [["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve"], [document.createElement('div'), document.createElement('div')]],
-"Artwork" : [["Dragon","Models","Pixel Art","Sketches","Boats","Environment","Anisa","Hi"], [document.createElement('div'), document.createElement('div')]],
-"Info" : [[],[document.createElement('div'), document.createElement('div')]]
+"0" : [["One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve"], [document.createElement('div'), document.createElement('div')]],
+"1" : [["Dragon","Models","Pixel Art","Sketches","Boats","Environment","Anisa","Hi"], [document.createElement('div'), document.createElement('div')]],
+"2" : [["CV", "Contacts", "Socials"],[document.createElement('div'), document.createElement('div')]]
 
 };
 
@@ -22,22 +21,47 @@ for(var i = 0; i < category.length; ++ i){
         }   
         document.getElementById(''+evt.currentTarget.my).style.display = 'block';
         category[evt.currentTarget.my].id = "selected";
+
+        document.querySelector("[id^='i']").replaceWith(categories[''+evt.currentTarget.my][1][0]);
+        document.querySelector("[id^='cb']").replaceWith(categories[''+evt.currentTarget.my][1][1]);
+
     });
 
     category[i].my = i;
 }
 
-for (var i = 0; i < categories["Games"][0].length; i++){
-    const game = document.createElement("div");
-    const box = document.createElement("div");
-    game.innerText = categories["Games"][0][i];
-    game.className = "hoverable";
-    box.id = "box";
+for(const [key, value] of Object.entries(categories)){
+    for (var i = 0; i < value[0].length; i++){
+        const game = document.createElement("div");
+        const box = document.createElement("div");
+        game.innerText = value[0][i];
+        game.className = "hoverable";
+        box.id = "box";
 
-    items.appendChild(game);
-    checkboxes.appendChild(box);
-    
+        value[1][0].className = "items";
+        value[1][0].id = "i";
+        value[1][0].appendChild(game);
+        value[1][1].className = "checkbox";
+        value[1][1].id = "cb";
+        value[1][1].appendChild(box);       
+    }
 }
+
+// for (var i = 0; i < categories["0"][0].length; i++){
+//     const game = document.createElement("div");
+//     const box = document.createElement("div");
+//     game.innerText = categories["0"][0][i];
+//     game.className = "hoverable";
+//     box.id = "box";
+
+//     categories["0"][1][0].className = "items";
+//     categories["0"][1][0].id = "i";
+//     categories["0"][1][0].appendChild(game);
+//     categories["0"][1][1].className = "checkbox";
+//     categories["0"][1][1].id = "cb";
+//     categories["0"][1][1].appendChild(box);
+    
+// }
 
 category[0].click();
 /*if (window.matchMedia( "(hover: none)" ).matches) {
