903ec915215a58b6baaa9038fd53be12cd786d06
YON <58594286+Yon-ln@users.noreply.github.com>
2022-11-09 20:18:46 +0000

    correct scroll

--- javascript.js
+++ javascript.js
@@ -1,7 +1,10 @@
 let activeIndex = 0;
-const maxDist = 40;
+let previousIndex = activeIndex;
+const maxDist = 100;
 let distance = 0;
+let scrolling = false;
 const groups = document.getElementsByClassName("main-card");
+let g = [groups[0], groups[1], groups[2]];
 
 let locked = false;
 let lastCall = false;
@@ -17,17 +20,30 @@ function returnIndex(dist){
     return 0;
 }
 
-window.addEventListener("wheel", event => {
+function setIndex(index){
+    const currentGroup = document.querySelector("[data-index=" + CSS.escape(activeIndex) + "]");
+    const nextGroup = document.querySelector("[data-index=" + CSS.escape(index) + "]");
+
+    if(previousIndex != index){
+
+        let n = g.shift();
+        g.push(n);
+
+        for(let i = 0; i < g.length; ++i){
+            g[i].dataset.trueIndex = i;
+        }
+    }
+
+    activeIndex = index;
+    previousIndex = activeIndex;
+
+}
+
+
+window.addEventListener("wheel", function (event) {
     distance = distance + Math.sign(event.deltaY) <= maxDist - 1 ? distance + Math.sign(event.deltaY) : 0;
     if(distance < 0) distance = maxDist;
 
-    const nextIndex = returnIndex(distance);
-    const currentGroup = document.querySelector("[data-index=" + CSS.escape(activeIndex) + "]");
-    const nextGroup = document.querySelector("[data-index=" + CSS.escape(nextIndex) + "]");
+    setIndex(returnIndex(distance));
 
-    currentGroup.dataset.status = "after";
-    nextGroup.dataset.status = "active";
-
-    activeIndex = nextIndex;
-    console.log(nextIndex);
-});
+}, false);
