1aff60ea78cde911d5a370942bd47e5812710b73
YON <58594286+Yon-ln@users.noreply.github.com>
2022-11-10 03:07:40 +0000

    backwards rotation and scrollwheel
    check direction of mouse drag

--- javascript.js
+++ javascript.js
@@ -1,47 +1,44 @@
 let activeIndex = 0;
 let previousIndex = activeIndex;
-const maxDist = 60;
+const maxDist = 40;
 let distance = 0;
 const groups = document.getElementsByClassName("main-card");
 const bar = document.getElementsByClassName("bar");
-bar[0].style.height = (1/groups.length) * 100 + "%";
+let _1vh = Math.round(window.innerHeight / (groups.length * 100));
+
+bar[0].style.height = (0.95*bar[0].parentNode.clientHeight) / groups.length + "px";
 
 let g = [groups[0], groups[1], groups[2]].sort((a, b) => parseFloat(a.dataset.trueIndex) - parseFloat(b.dataset.trueIndex));
 
 function returnIndex(dist){
-    const multiplier = 1/groups.length;
-
-    if(dist > multiplier * 2 * maxDist)
-        return 2;
-    else if(dist > multiplier * maxDist)
-        return 1;
-
-    return 0;
+    for(let i = groups.length-1; i >= 0; i--)
+        if(dist >= (1/groups.length) * i * maxDist)
+            return i;
 }
 
-function setIndex(index){
+function setIndex(index, dy){
     const currentGroup = document.querySelector("[data-index=" + CSS.escape(activeIndex) + "]");
     const nextGroup = document.querySelector("[data-index=" + CSS.escape(index) + "]");
 
-    if(previousIndex != index){
-        let n = g.shift();
-        g.push(n);
+    if(dy == -1){
+        let f = g.pop();
+        g.splice(g, 0, f);
+    }
+    else if(dy == 1){
+        let l = g.shift();
+        g.push(l);
     }
 
-    for(let i = 0; i < g.length; ++i){
-        g[i].dataset.trueIndex = i;
-    }
+    for(let i = 0; i < g.length; ++i) g[i].dataset.trueIndex = i;
     
-    previousIndex = activeIndex;
-    bar[0].style.marginTop = 1.85 * (index/groups.length) * 100 + "px";
-
+    bar[0].style.marginTop = index * (bar[0].parentNode.clientHeight / groups.length) + "px";
     activeIndex = index;
-
+    previousIndex = activeIndex;
 }
 
-function getToIndex(index){
+function getToIndex(index, dy){
     while (activeIndex != index){
-        setIndex(index);
+        setIndex(index, dy);
     }
 }
 
@@ -49,6 +46,12 @@ window.addEventListener("wheel", function (event) {
     distance = distance + Math.sign(event.deltaY) <= maxDist - 1 ? distance + Math.sign(event.deltaY) : 0;
     if(distance < 0) distance = maxDist;
 
-    getToIndex(returnIndex(distance));
+    let dy = event.deltaY < 0 ? -1 : 1;
+
+    getToIndex(returnIndex(distance), dy);
 
 }, false);
+
+document.getElementsByClassName("bar")[0].addEventListener('click', function (event){
+
+});
