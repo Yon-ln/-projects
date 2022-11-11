4d315d13e4cf707af968abb3a2ed2486d6291740
YON <58594286+Yon-ln@users.noreply.github.com>
2022-11-11 17:22:53 +0000

    a

--- generateData.js
+++ generateData.js
@@ -8,18 +8,182 @@ let index = {
     "previous" : 0,
     "max" : 0,
 };
-
-let data = null;
-
-$.ajax({
-    'async': false,
-    'global': false,
-    'url': "/data.json",
-    'dataType': "json",
-    'success': function (d) {
-        data = d;
+let data = { "cards" : [
+    {
+        "header" : [
+          "{img}home.svg",
+          "{p}Doanh Luu Nguyen"
+          ],
+  
+        "main" : [
+          "{p}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  
+          ],
+        
+        "footer" : [
+          "{img}instagram.svg",
+          "{img}twitch.svg",
+          "{img}twitter.svg",
+          "{img}linkedin.svg",
+          "{index}0"
+          ],
+  
+        "{data}trueIndex" : ""
+    },
+    {
+      "header" : [
+        "{img}python.png",
+        "{p}Specialising in AI"
+        ],
+  
+      "main" : [
+        "{p}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  
+        ],
+      
+      "footer" : [
+        "{img}instagram.svg",
+        "{img}twitch.svg",
+        "{img}twitter.svg",
+        "{img}linkedin.svg",
+        "{index}1"
+        ],
+  
+        "{data}trueIndex" : ""
+    },
+    {
+      "header" : [
+        "{img}home.svg",
+        "{p}Doanh Luu Nguyen"
+        ],
+  
+      "main" : [
+        "{p}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  
+        ],
+      
+      "footer" : [
+        "{img}instagram.svg",
+        "{img}twitch.svg",
+        "{img}twitter.svg",
+        "{img}linkedin.svg"
+        ],
+        
+        "{data}trueIndex" : ""
+    },
+    {
+      "header" : [
+        "{img}home.svg",
+        "{p}Doanh Luu Nguyen"
+        ],
+  
+      "main" : [
+        "{p}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  
+        ],
+      
+      "footer" : [
+        "{img}instagram.svg",
+        "{img}twitch.svg",
+        "{img}twitter.svg",
+        "{img}linkedin.svg"
+        ],
+        
+        "{data}trueIndex" : ""
+    },
+    {
+      "header" : [
+        "{img}home.svg",
+        "{p}Doanh Luu Nguyen"
+        ],
+  
+      "main" : [
+        "{p}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  
+        ],
+      
+      "footer" : [
+        "{img}instagram.svg",
+        "{img}twitch.svg",
+        "{img}twitter.svg",
+        "{img}linkedin.svg"
+        ],
+        
+        "{data}trueIndex" : ""
+    },
+    {
+      "header" : [
+        "{img}home.svg",
+        "{p}Doanh Luu Nguyen"
+        ],
+  
+      "main" : [
+        "{p}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  
+        ],
+      
+      "footer" : [
+        "{img}instagram.svg",
+        "{img}twitch.svg",
+        "{img}twitter.svg",
+        "{img}linkedin.svg"
+        ],
+        
+        "{data}trueIndex" : ""
+    },
+    {
+      "header" : [
+        "{img}home.svg",
+        "{p}Doanh Luu Nguyen"
+        ],
+  
+      "main" : [
+        "{p}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  
+        ],
+      
+      "footer" : [
+        "{img}instagram.svg",
+        "{img}twitch.svg",
+        "{img}twitter.svg",
+        "{img}linkedin.svg"
+        ],
+        
+        "{data}trueIndex" : ""
+    },
+    {
+      "header" : [
+        "{img}home.svg",
+        "{p}Doanh Luu Nguyen"
+        ],
+  
+      "main" : [
+        "{p}Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  
+        ],
+      
+      "footer" : [
+        "{img}instagram.svg",
+        "{img}twitch.svg",
+        "{img}twitter.svg",
+        "{img}linkedin.svg"
+        ],
+        
+        "{data}trueIndex" : ""
     }
-});
+  
+  ]};
+// let data = null;
+
+// $.ajax({
+//     'async': false,
+//     'global': false,
+//     'url': "/data.json",
+//     'dataType': "json",
+//     'success': function (d) {
+//         data = d;
+//     }
+// });
 
 const keywords = {
     "{class}" : 0,
